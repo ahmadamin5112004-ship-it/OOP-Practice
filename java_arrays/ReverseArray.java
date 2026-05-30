8b5a93ff4565c88ea1942517fd7fca32d8f0cd4b
ahmadamin5112004-ship-it <ahmadamin5112004@gmail.com>
2026-05-30 19:55:36 +0600

    solved

---
+++ java_arrays/ReverseArray.java
@@ -0,0 +1,27 @@
+package java_arrays;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Reversed series:");
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
